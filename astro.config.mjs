3434dced8ebd41f057732878fa91be7c6231f1cb
Jean-Christophe Cuvelier <jcc@cuvelier.co>
2026-04-28 12:57:49 +0000

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   output: 'static',
   site: 'https://wellmade.be',
   vite: {
